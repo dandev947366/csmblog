01ac357c4df29ba236e30341ea6281c13c4ef8e7
danofoundation <dan.dev947366@gmail.com>
2024-09-08 18:53:48 +0300

    Add react hook form

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,8 +15,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Dan Le',
+            'email' => 'danle@gmail.com',
+            'password' => Hash::make('password'),
+            'address' => "Helsinki, Finland",
+            'phone' => "123234546"
         ]);
     }
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,25 @@
+import { useForm, SubmitHandler } from "react-hook-form"
 
+type Input = {
+  email: string,
+  password: string
+}
 function Login() {
+  const {register, handleSubmit, watch, formState:{errors}} = useForm();
+  const login: SubmitHandler<Inputs> = data => console.log(data)
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
     <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
       <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
-      <form action="">
+      <form onSubmit={handleSubmit(login)}>
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700 text-sm py-2 font-bold">Email:</label>
-          <input type="text" id="email" placeholder="Enter email" className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring focus-blue-200 h-8"/>
+          <input type="text" id="email" placeholder="Enter email" className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring focus-blue-200 h-8" {...register("email", {required: true})}/>{errors.email && <span>Email required</span>}
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="block text-gray-700 text-sm py-2 font-bold">Password:</label>
-          <input type="text" id="password" placeholder="Enter password" className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring focus-blue-200 h-8"/>
+          <input type="text" id="password" placeholder="Enter password" className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring focus-blue-200 h-8" {...register("password", {required: true})}/>
+          {errors.password && <span>Password required</span>}
         </div>
         <div className="mb-6">
           <button type="submit" className="w-full p-2 rounded bg-blue-500 text-white hover:bg-blue-700">Submit</button>
