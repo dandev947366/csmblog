aaa30c9c7f3d4a72bb6967cb4d6a6f168812c03e
danofoundation <dan.dev947366@gmail.com>
2024-09-13 10:04:28 +0300

    Update

---
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+
+    }
+
+    public function login(){
+        return 1;
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,5 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/login', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\V1\AuthController;
+Route::get('/v1/login', [AuthController::class, 'login']);
